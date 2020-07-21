cc1970274fff27fa5b50cb8258531104ca5e14ec
Pratiksha Bhangdiya <pratiksha@Amols-MacBook-Air.local>
2020-07-20 19:38:28 -0700

    baech resort application

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Hero from "../components/Hero";
+//import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import { Link } from "react-router-dom";
 import defaultBcg from "../images/room-1.jpeg";
